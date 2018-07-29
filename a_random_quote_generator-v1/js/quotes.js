d6520c788ec3d06e32af2a26aa9080321c5d03da
kshields51 <kshields551@gmail.com>
2018-07-29 19:13:46 -0400

    flawed but working
    prints many messages need to clean

--- a_random_quote_generator-v1/js/quotes.js
+++ a_random_quote_generator-v1/js/quotes.js
@@ -1,32 +1,32 @@
 var quotes = [
     {
-        quote: "quote goes here",
-        source: "source goes here",
-        citation: "citation goes here",
-        year: "year goes here"
+        quote: "Fourscore and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.",
+        source: "Abraham Lincoln",
+        citation: "Gettysburg Address",
+        year: "Novemeber 19th, 1863"
     },
     {
-        quote: "quote goes here",
-        source: "source goes here",
-        citation: "citation goes here",
-        year: "year goes here"
+        quote: "The Earth is a very small stage in a vast cosmic arena.",
+        source: "Carl Sagan",
+        citation: "None",
+        year: "None"
     },
     {
-        quote: "quote goes here",
-        source: "source goes here",
-        citation: "citation goes here",
-        year: "year goes here"
+        quote: "Ain't gonna be no rematch...ain't gonna be no rematch.",
+        source: "Apollo Creed",
+        citation: "Rocky",
+        year: "1976"
     },
     {
-        quote: "quote goes here",
-        source: "source goes here",
-        citation: "citation goes here",
-        year: "year goes here"
+        quote: "See ya in another life, brother.",
+        source: "Desmond Hume",
+        citation: "Lost",
+        year: "None"
     },
     {
-        quote: "quote goes here",
-        source: "source goes here",
-        citation: "citation goes here",
-        year: "year goes here"
+        quote: "I'll be back.",
+        source: "Arnold Schwarzenegger",
+        citation: "The Terminator",
+        year: "1984"
     }
 ]
--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -1,34 +1,50 @@
 // FSJS - Random Quote Generator
 
-// Create the array of quote objects and name it quotes 
-
-
-
-
-               
-
-/*
+// Create the array of quote objects and name it quotes complete see other file as per vidoe
+/* here is an alternate way i was thinking of doing it
 person2: ["quote goes here", "Here goes the location"],
 person3: ["quote goes here", "Here goes the location"],
 person4: ["quote goes here", "Here goes the location"],
 person5: ["quote goes here", "Here goes the location"]
 }
 */
-
-
-
-
 // Create the getRandomQuuote function and name it getRandomQuote
+
+var message = ''
 function getRandomQuote (array) {
-var randomNumber = Math.floor( Math.random() * 5 ) + 1
-
+var randomNumber = Math.floor( Math.random() * 5 ) // generates the random number
+return array[randomNumber] //this returns a quote object to pass into the print quote
 }
-
-
 // Create the printQuote funtion and name it printQuote
+function printQuote (message) { 
+   var gatheredQuote = getRandomQuote(quotes)
+   
+
+   for (prop in gatheredQuote) {
+    message+=' <p class="quote">' + gatheredQuote['quote'] + '</p>\
+  <p class="source">' + gatheredQuote['source'] + '\
+    <span class="citation">' + gatheredQuote['citation'] + '</span>\
+    <span class="year">' + gatheredQuote['year'] + '</span>\
+  </p>';
+
+   var outputDiv = document.getElementById('quote-box').innerHTML = message;
+
+   document.write(outputDiv)
+   //loop hough the quotes in gathered Quote and appened to this
+   
+
+
+  }
+   }
+
+
+printQuote(message)
+
+
+
 
 
 
 // This event listener will respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
