ba7a6bea31c0a705038087a80ff8c7d6a47e898d
kshields51 <kshields551@gmail.com>
2018-07-29 21:59:52 -0400

    complete before clean
    just need to add good comments

--- a_random_quote_generator-v1/js/quotes.js
+++ a_random_quote_generator-v1/js/quotes.js
@@ -3,11 +3,13 @@ var quotes = [
         quote: "Fourscore and seven years ago our fathers brought forth on this continent, a new nation, conceived in Liberty, and dedicated to the proposition that all men are created equal.",
         source: "Abraham Lincoln",
         citation: "Gettysburg Address",
-        year: "Novemeber 19th, 1863"
+        year: "Novemeber 19th, 1863",
+        type: "history"
     },
     {
         quote: "The Earth is a very small stage in a vast cosmic arena.",
-        source: "Carl Sagan"
+        source: "Carl Sagan",
+        type: "science"
         
         
     },
@@ -15,18 +17,23 @@ var quotes = [
         quote: "Ain't gonna be no rematch...ain't gonna be no rematch.",
         source: "Apollo Creed",
         citation: "Rocky",
-        year: "1976"
+        year: "1976",
+        type: "movie"
     },
     {
         quote: "See ya in another life, brother.",
         source: "Desmond Hume",
-        citation: "Lost"
+        citation: "Lost",
+        type: "tv"
         
     },
     {
         quote: "I'll be back.",
         source: "Arnold Schwarzenegger",
         citation: "The Terminator",
-        year: "1984"
+        year: "1984",
     }
 ]
+
+//note the Arnold Schwarzenegger quote does not have 'type' to demonstrate\
+//that the if statement works for it
--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -9,11 +9,18 @@ person5: ["quote goes here", "Here goes the location"]
 }
 */
 // Create the getRandomQuuote function and name it getRandomQuote
-
 var message = ''
+var colors = ['#ff1848', '#00a100', "blue"]
 function getRandomQuote (array) {
 var randomNumber = Math.floor( Math.random() * 5 ) // generates the random number
 return array[randomNumber] //this returns a quote object to pass into the print quote
+}
+//this function randomly changes the background color and is 
+//activated when the user clicks the button or when
+//a new quote is automatically generated
+function randomBackground(){
+  number = Math.floor((Math.random()* 3));
+  document.body.style.backgroundColor = colors[number]
 }
 // Create the printQuote funtion and name it printQuote
 function printQuote (message) { 
@@ -34,36 +41,27 @@ function printQuote (message) {
   } else{
     message +='<span class="year">' + gatheredQuote['year'] + '</span>\
     </p>'}
+  if ("undefined" === typeof(gatheredQuote['type'])) {
 
-  
-  
-  
-  
-  /*
-  + '\
-    <span class="citation">' + gatheredQuote['citation'] + '</span>\
-    <span class="year">' + gatheredQuote['year'] + '</span>\
-  </p>';
-  
-*/
+  } else{
+    message +='<span class="type">' + " " + "(" +gatheredQuote['type']+')'+'</span></p>'}
+
+
 
    var outputDiv = document.getElementById('quote-box').innerHTML = message;
+   randomBackground()
 
    //loop hough the quotes in gathered Quote and appened to this
    
 
 
   }
+ 
+  
+    
    }
-
-
 printQuote(message)
-
-
-
-
-
-
 // This event listener will respond to "Show another quote" button clicks
 //when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+var interval = setInterval(printQuote, 5000);
